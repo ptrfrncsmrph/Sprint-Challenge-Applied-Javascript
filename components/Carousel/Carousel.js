a0d5837302dc069311ecf676311fc3290050c121
Peter Murphy <peter.francis.murphy@gmail.com>
2019-02-01 14:24:06 -0500

    Refactors to use functions; memory leaks like a sieve

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,43 +1,20 @@
-// `mod` is based on Donald Knuth's modular division definition (remainder has sign of dividend)
-const mod = (x, y) => x - y * Math.floor(x / y)
+;(() => {
+  // `mod` is based on Donald Knuth's modular division definition (remainder has sign of dividend)
+  const mod = (x, y) => x - y * Math.floor(x / y)
 
-class Carousel {
-  constructor(element) {
-    this.carousel = element
-    this.index = 0
-    this.imgList = this.carousel.querySelectorAll("img")
-    this.length = this.imgList.length
-    this.controls = [...this.carousel.querySelectorAll("[class$='button']")]
-    ;(([l, r]) => {
-      l.addEventListener("click", () =>
-        this.render(mod(this.index - 1, this.length), this.index)
-      )
-      r.addEventListener("click", () =>
-        this.render(mod(this.index + 1, this.length), this.index)
-      )
-    })(this.controls)
-    this.render(this.index)
+  const carousel = document.querySelector(".carousel")
+  const imgList = carousel.querySelectorAll("img")
+  const length = imgList.length
+  const [left, right] = [...carousel.querySelectorAll("[class$='button']")]
+  const run = (current = 0, last) => {
+    last !== undefined && (imgList[last].style.display = "none")
+    imgList[current].style.display = "block"
+    left.addEventListener("click", () => {
+      run(mod(current - 1, length), current)
+    })
+    right.addEventListener("click", () => {
+      run(mod(current + 1, length), current)
+    })
   }
-
-  static of(element) {
-    return new Carousel(element)
-  }
-
-  render(current, last) {
-    last !== undefined && (this.imgList[last].style.display = "none")
-    this.imgList[current].style.display = "block"
-    this.index = current
-  }
-}
-
-const carousel = document.querySelector(".carousel")
-Carousel.of(carousel)
-
-/* If You've gotten this far, you're on your own! Although we will give you some hints:
-    1. You will need to grab a reference to the carousel, and in it grab the laft and right buttons
-    2. You will need to grab a reference to all of the images
-    3. Create a current index
-    4. Those buttons are gonna need some click handlers.
-    5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
-    6. Have fun!
-*/
+  run()
+})()
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,34 +1,36 @@
-const tabs = document.querySelectorAll(".tab")
-const cards = document.querySelectorAll(".card")
+;(() => {
+  const tabs = document.querySelectorAll(".tab")
+  const cards = document.querySelectorAll(".card")
 
-const show = tab => {
-  cards.forEach(({ dataset, style }) => {
-    tab === "all"
-      ? (style.display = "flex")
-      : dataset.tab === tab && (style.display = "flex")
-  })
-  tabs.forEach(({ dataset, classList }) => {
-    dataset.tab === tab && classList.add("active-tab")
-  })
-}
+  const show = tab => {
+    cards.forEach(({ dataset, style }) => {
+      tab === "all"
+        ? (style.display = "flex")
+        : dataset.tab === tab && (style.display = "flex")
+    })
+    tabs.forEach(({ dataset, classList }) => {
+      dataset.tab === tab && classList.add("active-tab")
+    })
+  }
 
-const hide = tab => {
-  cards.forEach(({ dataset, style }) => {
-    tab === "all"
-      ? (style.display = "none")
-      : dataset.tab === tab && (style.display = "none")
-  })
-  tabs.forEach(({ dataset, classList }) => {
-    dataset.tab === tab && classList.remove("active-tab")
-  })
-}
+  const hide = tab => {
+    cards.forEach(({ dataset, style }) => {
+      tab === "all"
+        ? (style.display = "none")
+        : dataset.tab === tab && (style.display = "none")
+    })
+    tabs.forEach(({ dataset, classList }) => {
+      dataset.tab === tab && classList.remove("active-tab")
+    })
+  }
 
-const run = (current = "all", last) => {
-  last && hide(last)
-  show(current)
-  tabs.forEach(tab => {
-    tab.addEventListener("click", () => run(tab.dataset.tab, current))
-  })
-}
+  const run = (current = "all", last) => {
+    last && hide(last)
+    show(current)
+    tabs.forEach(tab => {
+      tab.addEventListener("click", () => run(tab.dataset.tab, current))
+    })
+  }
 
-run()
+  run()
+})()
